6f259ce721a041faf951f0babd5fb495353aa6e6
IvanKalnitsky <81367158+IvanKalnitsky@users.noreply.github.com>
2021-07-11 20:52:01 +0300

    Initial Commit

---
+++ SourceThreeTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SourceThreeTest
+//
+//  Created by macbookp on 11.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
